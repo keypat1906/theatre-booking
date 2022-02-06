4ffe1b28803e6e86bcadf5b8b7bd59a7d4ab2dd7
keypat1906 <keypat1906@gmail.com>
2022-02-06 13:59:49 -0600

    Added requirement.txt

--- urlshortening/forms.py
+++ urlshortening/forms.py
@@ -3,8 +3,6 @@ from django import forms
 from .models import Url
 
 from django import forms
-from django.utils.translation import ugettext_lazy as _          
-from django.core.validators import RegexValidator
 
 
 class ShortUrlForm(forms.ModelForm):
